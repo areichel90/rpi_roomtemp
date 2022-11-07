644ae011ba9c1d9f39752f3ae1b28c136936dd0b
Andrew Reichel <andrew.reichel@gmail.com>
2022-11-06 21:43:31 -0500

    :potato:

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-import temp_utils
-from temp_utils import *
+import temp_utils, time, threading, sys, os
 
-def testFunc():
+
+
+def runMeasurement(sample_rate, out_path):
+    # configure method to be called at frequency equal to sample_rate
+    print("\n")
     test_meas = temp_utils.measurement()
+    test_meas.test = threading.Timer(sample_rate, measureTemperature, args=(sample_rate,out_path)).start()
+
+    # perform measurement and store information in new class obj
+    new_reading = 10.0  # TODO: sample from sensor here - should this be written into class?
+    test_meas.temp = new_reading
 
     print(vars(test_meas))
 
+    # write measurement to file
+    test_meas.write_meas(out_path)
+
+
+
+
 
 if __name__ == "__main__":
-    print("running")
+    print(f"running: {__file__}")
 
-    testFunc()
+    ''' setup  - check that data log exists, if not - create one '''
+    fn = "temp_log"
+    # TODO: search for file, if none found, create it
+    out_path = os.path.join(__file__, fn)
 
-    print("run complete.")
+    ''' periodically, take sensor measurement and write it to file '''
+    sample_rate = 1.0  # sec
+    temp_meas = runMeasurement(sample_rate, out_path)
+
--- temp_utils.py
+++ temp_utils.py
@@ -1,8 +1,10 @@
-
+from datetime import datetime
 
 class measurement:
     ''' <> '''
 
     def __init__(self):
-        print("here")
+        self.time = datetime.now().time()
 
+    def write_meas(self, fp_out:str):
+        print(f"writing measurement to {fp_out}")
