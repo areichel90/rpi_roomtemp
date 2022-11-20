ffc17df502a13d895f0fbe4c12d3cc16b32dda41
areichel90 <andrew.reichel@gmail.com>
2022-11-20 04:06:34 +0000

    setting up dht lib and simple sample script

--- temp_utils.py
+++ temp_utils.py
@@ -1,10 +1,25 @@
 from datetime import datetime
+import adafruit_dht
+from board import D4
 
 class measurement:
     ''' <> '''
 
     def __init__(self):
+        print("\nTaking Measurement...")
         self.time = datetime.now().time()
+        #self.take_measurement()
 
-    def write_meas(self, fp_out:str):
-        print(f"writing measurement to {fp_out}")
+    def take_measurement(self):
+        dht_device = adafruit_dht.DHT22(D4)
+        self.temperature = dht_device.temperature
+        self.humidity = dht_device.humidity
+
+
+
+if __name__ == "__main__":
+    print("Starting Sensor Measurement File.")
+    
+    meas = measurement()
+    
+    print("Script Completed Successfully.")
